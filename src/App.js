8aad9671c3ba344932e1c82b69cdd7ba71a5dd97
yuki-kasai <yuki000kasai000@gmail.com>
2020-07-09 16:43:12 +0900

    add component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let items = [
-    {"name": "いちご", "price": "100"},
-    {"name": "りんご", "price": "150"},
-    {"name": "バナナ", "price": "230"}
-  ]
-
   return (
-    <div className="App container">
-      <table className="table table-striped">
-        <tbody>
-          {items.map((value) => (
-            <tr>
-              <th scope="row">{value.name}</th>
-              <td>{value.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container text-center">
+      <Clock />
     </div>
   )
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    // React.Componentの初期化処理を行う
+    super(props);
+    this.now = new Date();
+
+    this.state = {
+      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+  refresh() {
+    this.now = new Date();
+
+     this.setState((state) => ({
+       time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+     }));
+  }
+
+  render() {
+    return <p onClick={this.refresh}>{this.state.time}</p>
+  }
 }
 
 export default App;
